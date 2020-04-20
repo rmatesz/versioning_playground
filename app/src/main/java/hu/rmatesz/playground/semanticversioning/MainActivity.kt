aba105764859a135232e7fc699d1eba9439619b3
Mate Redecsi <>
2020-04-20 13:56:59 +0200

    BREAKING CHANGE: Setup semantic versioning properly for Android.

--- app/src/main/java/hu/rmatesz/playground/semanticversioning/MainActivity.kt
+++ app/src/main/java/hu/rmatesz/playground/semanticversioning/MainActivity.kt
@@ -2,11 +2,15 @@ package hu.rmatesz.playground.semanticversioning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.version).apply {
+            text = "Version: ${BuildConfig.VERSION_NAME}; code: ${BuildConfig.VERSION_CODE}"
+        }
     }
 }
